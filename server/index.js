67e5cbbbd42d6983d1be2f7ba279e1d2c5c8613d
Atul semwal <atulsemwal332@gmail.com>
2025-06-11 11:57:26 +0530

    db add

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
-const express = require("express")
-
+import express from "express"
+import ConnectDb from "./db/db.js";
 const app = express();
 
+app.use('/', (req , res)=>{
+    res.send("This jslkjdkljfkljdg")
+})
 
+ConnectDb()
 const PORT= 3888
 app.listen(PORT , ()=>{
     console.log(`server is running on port : ${PORT}`)
