97312ba2eb6e28804c9b1fbea16e1c5a77821acc
Atul semwal <atulsemwal332@gmail.com>
2025-06-23 18:31:20 +0530

    token

--- server/Middleware/authMiddle.js
+++ server/Middleware/authMiddle.js
@@ -2,16 +2,17 @@ const jwt = require("jsonwebtoken");
 const SECRET_KEY = process.env.JWT_SECRET || "mySuperSecretKey";
 
 const verifyAdmin = (req, res, next) => {
-  const token = req.cookies.adminToken;
+  const {adminToken} = req.cookies;
+  // console.log(adminToken)
 
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  if (!adminToken) return res.status(401).json({ message: "Unauthorized" });
 
   try {
-    const decoded = jwt.verify(token, SECRET_KEY);
+    const decoded = jwt.verify(adminToken, SECRET_KEY);
     req.adminId = decoded.id;
     next();
   } catch (err) {
-    return res.status(401).json({ message: "Invalid Token" });
+    return res.status(401).json({ message: "Invalid adminToken" });
   }
 };
 
